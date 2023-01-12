3cd929fe21df1bc462cca327f4fa56c11a107058
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-13 03:01:35 +0900

    test7

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ then
 fi
 
 cd /home/ubuntu/srv/ubuntu && git submodule update --init --recursive
-
+mv /home/ubuntu/srv/ubuntu/.env /home/ubuntu/srv/ubuntu/backend/
 echo "start docker-compose up: ubuntu"
 sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose.yml up --build -d
