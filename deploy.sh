aacd9a5bd252ce9e635c12dee6b7b38bb5c45c4b
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-19 19:09:42 +0900

    deploy 수정

--- deploy.sh
+++ deploy.sh
@@ -22,8 +22,10 @@ then
   sudo curl -L "https://github.com/docker/compose/releases/download/1.27.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
-
-cd /home/ubuntu/srv/ubuntu && git submodule init && git submodule update --recursive --remote --merge
-mv /home/ubuntu/srv/ubuntu/.env /home/ubuntu/srv/ubuntu/backend/
+echo "start submoudle update"
+cd /home/ubuntu/srv/ubuntu
+sudo git submodule init
+sudo submodule update --recursive --remote --merge
+sudo mv /home/ubuntu/srv/ubuntu/.env /home/ubuntu/srv/ubuntu/backend/
 echo "start docker-compose up: ubuntu"
 sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose.yml up --build -d
