7d65080445456d8a9d646597534c695101188e0f
Kyongmin1016 <rkdrnwkd1@gmail.com>
2023-01-19 18:46:52 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ then
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
-cd /home/ubuntu/srv/ubuntu && git submodule update --recursive --remote --merge
+cd /home/ubuntu/srv/ubuntu && git submodule update
 mv /home/ubuntu/srv/ubuntu/.env /home/ubuntu/srv/ubuntu/backend/
 echo "start docker-compose up: ubuntu"
 sudo docker-compose -f /home/ubuntu/srv/ubuntu/docker-compose.yml up --build -d
